f2a90acce5951f61151c5dada6c69722c2402cc0
Martin Strobel <martin.strobel@live.com>
2020-09-16 14:47:55 -0700

    Adding persist.Cache type.
    This can act as a stand-alone in-memory Loader/Writer, or act as a
    decorator.

---
+++ persist/cache_test.go
@@ -0,0 +1,78 @@
+package persist
+
+import (
+	"context"
+	"github.com/marstr/envelopes"
+	"testing"
+)
+
+func TestCache_Load_passThroughMiss(t *testing.T) {
+	ctx := context.Background()
+	const rawTargetId = "07e72edcf913fd3ef5eababf60852216d68dbb90"
+	var targetId envelopes.ID
+	targetId.UnmarshalText([]byte(rawTargetId))
+
+	passThrough := FileSystem{
+		Root: "./testdata/test3/.baronial",
+	}
+
+	subject := NewCache(10)
+	subject.Loader = &DefaultLoader{
+		Fetcher: passThrough,
+	}
+
+	var got envelopes.State
+	err := subject.Load(ctx, targetId, &got)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	var want envelopes.State
+	err = subject.Loader.Load(ctx, targetId, &want)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if !got.Equal(want) {
+		t.Logf("got: %s, want: %s", got.String(), want.String())
+		t.Fail()
+	}
+}
+
+func TestCache_Load_reuseHits(t *testing.T) {
+	var err error
+	ctx := context.Background()
+	const rawTargetId = "07e72edcf913fd3ef5eababf60852216d68dbb90"
+	var targetId envelopes.ID
+	targetId.UnmarshalText([]byte(rawTargetId))
+
+	passThrough := FileSystem{
+		Root: "./testdata/test3/.baronial",
+	}
+
+	subject := NewCache(10)
+	subject.Loader = &DefaultLoader{
+		Fetcher: passThrough,
+	}
+
+	var want envelopes.State
+	err = subject.Load(ctx, targetId, &want)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	var got envelopes.State
+	err = subject.Load(ctx, targetId, &got)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if got.Budget != want.Budget {
+		t.Logf("When encountering a cache hit, the SAME Budget object should be reused")
+		t.Fail()
+	}
+}
